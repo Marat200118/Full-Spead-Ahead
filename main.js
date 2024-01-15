4afa1dc5df304e92ce1229e2777d1f6a64ddbce7
Marat200118 <marat.samigullin1@gmail.com>
2024-01-15 21:57:54 +0100

    added content + lottie animations integration

--- main.js
+++ main.js
@@ -1,2 +1,41 @@
-import "./css/style.css";
+const init = () => {
+  gsap.registerPlugin(ScrollTrigger);
+  pbkalAnimation();
+  lgvAnimation();
+  setupScrollTrigger();
+};
 
+const pbkalAnimation = () => {
+  lottie.loadAnimation({
+    container: document.querySelector(".pbkal-animation"),
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    path: "animations/pbkal.json",
+  });
+};
+
+const lgvAnimation = () => {
+  lottie.loadAnimation({
+    container: document.querySelector(".lgv-animation"),
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    path: "animations/lgv.json",
+  });
+};
+
+const setupScrollTrigger = () => {
+  gsap.from(".history", {
+    scrollTrigger: {
+      trigger: ".history",
+      start: "top bottom",
+      end: "center top",
+      scrub: true,
+    },
+    opacity: 0,
+    duration: 0.5,
+  });
+};
+
+init();
