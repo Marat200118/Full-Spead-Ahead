0cc3821b4ef38031d3e78a34eb0bdd041e6870d5
Marat200118 <marat.samigullin1@gmail.com>
2024-01-16 19:58:56 +0100

    added animations, experimenting with drag and drop

--- main.js
+++ main.js
@@ -2,8 +2,41 @@ const init = () => {
   gsap.registerPlugin(ScrollTrigger);
   pbkalAnimation();
   lgvAnimation();
+  leuvenAnimation();
+  hslFourAnimation();
   setupScrollTrigger();
+  dragAndDrop();
 };
+
+const dragAndDrop = () => {
+
+    const allowDrop = (event) => {
+      event.preventDefault();
+    };
+
+    const drag = (event) => {
+      event.dataTransfer.setData("text", event.target.id);
+    };
+
+    const drop = (event) => {
+      event.preventDefault();
+      var data = event.dataTransfer.getData("text");
+      event.target.appendChild(document.getElementById(data));
+    };
+
+    const shapes = document.querySelectorAll(".shapes div");
+    const targets = document.querySelectorAll(".document div");
+
+    shapes.forEach((shape) => {
+      shape.addEventListener("dragstart", drag);
+    });
+
+    // Set up the drop event listeners for the targets
+    targets.forEach((target) => {
+      target.addEventListener("dragover", allowDrop);
+      target.addEventListener("drop", drop);
+    });
+}
 
 const pbkalAnimation = () => {
   lottie.loadAnimation({
@@ -22,6 +55,26 @@ const lgvAnimation = () => {
     loop: true,
     autoplay: true,
     path: "animations/lgv.json",
+  });
+};
+
+const leuvenAnimation = () => {
+  lottie.loadAnimation({
+    container: document.querySelector(".leuven-animation"),
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    path: "animations/leuven.json",
+  });
+};
+
+const hslFourAnimation = () => {
+  lottie.loadAnimation({
+    container: document.querySelector(".hsl-4-animation"),
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    path: "animations/hsl4.json",
   });
 };
 
